d0c69bbf695bcebfa76b779661a52dc6e014f890
Muhamed Mehmedovic <54117777+Muhamed112@users.noreply.github.com>
2024-01-20 02:54:08 +0100

    test

--- helpers.ts
+++ helpers.ts
@@ -25,10 +25,8 @@ export function updateTaskStatus(
   status: TaskStatus,
   tasks: Task[]
 ): void {
-  //Add logic for update
   for (let i = 0; i < tasks.length; i++) {
     if (tasks[i][0] === id) {
-      //Conflict test
       tasks[i][2] = status;
     }
   }
